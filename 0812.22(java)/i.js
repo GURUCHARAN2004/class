8e0f423faaa1106f55e86c861b4ecf0797764ccf
GURUCHARAN2004 <118422331+GURUCHARAN2004@users.noreply.github.com>
2022-12-08 15:48:00 +0530

    Initial commit

---
+++ 0812.22(java)/i.js
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>basic java</title>
+</head>
+<body>
+<script>
+function newName(myName,dept){
+name1 =myName+' '+dept
+console.log(name1)
+alert(name1)
+document.write(name1)
+}
+newName('vimal', 'cse');
+</script>
+    
+</body>
+
+</html>
